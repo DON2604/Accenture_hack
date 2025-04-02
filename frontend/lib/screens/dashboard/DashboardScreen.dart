299873a0fc37767375a2011c00e753b29129cb36
Nidhi Sikder <nidhisbkp@gmail.com>
2025-04-02 17:48:44 +0530

    vertical_carousel

--- frontend/lib/screens/dashboard/DashboardScreen.dart
+++ frontend/lib/screens/dashboard/DashboardScreen.dart
@@ -29,7 +29,7 @@ class DashboardScreen extends StatelessWidget {
               ),
               SizedBox(height: 15),
               Expanded(child: ScrollableList()),
-              BottomNavBar(),
+              //BottomNavBar(),
             ],
           ),
         ],
